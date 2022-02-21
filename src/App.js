a95f7ca814e0d9a00ba4b407a01beb0387e5081d
Hensetsu <nothile1@gmail.com>
2022-02-21 18:52:30 +0000

    Updated text to test build

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Moyos Munch
+          Moyos Munch!!
         </a>
       </header>
     </div>
