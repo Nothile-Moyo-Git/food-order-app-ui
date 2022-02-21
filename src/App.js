e5656508901debd9798798b940b9d98929801c0b
Hensetsu <nothile1@gmail.com>
2022-02-21 20:27:35 +0000

    Going to rebuild my sass files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import Header from "./components/Layout/Header";
-
+import Header from './Components/Layout/Header';
+import logo from './logo.svg';
+import './styles/App.css';
 
 function App() {
   return (
-    <div>
-      <Header/>
+    <div className="App">
+      <header className="App-header">
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Moyos Munch!!!
+        </a>
+        <Header></Header>
+      </header>
     </div>
   );
 }
