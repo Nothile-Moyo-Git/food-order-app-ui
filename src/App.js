4d766bf765bec4b7e5aea9602418e2af6b1e4b99
Hensetsu <nothile1@gmail.com>
2022-02-21 20:22:36 +0000

    Uploaded previous sass project

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
-import Header from './Components/Layout/Header';
-import logo from './logo.svg';
-import './styles/App.css';
+import Header from "./components/Layout/Header";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Moyos Munch!!!
-        </a>
-        <Header></Header>
-      </header>
+    <div>
+      <Header/>
     </div>
   );
 }
--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -1,12 +1,22 @@
 import React from "react";
 import './Header.css'
+import HeaderCartButton from './HeaderCartButton.js'
 
 const Header = (props) => {
 
-    return(<>
-        <h2> Ross Kemp </h2>
-    </>);
+    console.log( 'Whats in our classes object' );
 
+return(
+    <header>
+        <div className="header">
+            <h2> Moyo's Munch </h2>
+            <HeaderCartButton></HeaderCartButton>
+        </div>
+
+        <div className={'main-image'}>
+            <img src={process.env.PUBLIC_URL + '/assets/food_2.jpg'} alt=""/>
+        </div>
+    </header>);
 }
 
 export default Header;
