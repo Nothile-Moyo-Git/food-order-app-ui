981fe1b30191cae1335c9ac7c407567a0de7dc54
Hensetsu <nothile1@gmail.com>
2022-02-24 20:32:45 +0000

    Removed app margin and fixed image overflow issue

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
         <Header></Header>
+        <div className="test"> Test content </div>
     </div>
   );
 }
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -6,14 +6,16 @@ const Header = (props) => {
 
     return(
         <>
+
         <header className="header">
             <h2> Moyo's Munch </h2>
             <HeaderCartButton></HeaderCartButton>
         </header>
-
+        
         <div className="main-image">
-         <img src={process.env.PUBLIC_URL + '/assets/food_2.jpg'} alt=""/>
+            <img src={process.env.PUBLIC_URL + '/assets/burger_and_chips.jpg'} alt=""/>
         </div>
+
         </>
     );
 
