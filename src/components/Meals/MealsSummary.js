46161e6e11955a3553284e412495e5634b2b2d06
Hensetsu <nothile1@gmail.com>
2022-02-26 19:01:02 +0000

    --

--- src/components/Meals/MealsSummary.js
+++ src/components/Meals/MealsSummary.js
@@ -5,7 +5,7 @@ const MealsSummary = (props) => {
 
     return(<div className="meals-summary">
             <h2>The best munch in town!</h2>
-            <p> Please choose a food option from my delicious menu below! </p>
+            <p> Please choose a food option from my delicious menu below! It's great :) </p>
             <p> Our meals are cooked by our experienced chefs, cooked to order and only use the healthiest and freshest ingredients </p>
     </div>);
 }
