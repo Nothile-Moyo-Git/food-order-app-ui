f3f0a17c6393f08444fd5fec04e9016b1b419bc2
Hensetsu <nothile1@gmail.com>
2022-03-15 20:00:59 +0000

    Added animations for app load and adding items to cart

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,9 +1,12 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import './HeaderCartButton.scss';
 import CartIcon from '../Cart/CartIcon';
 import CartContext from '../../store/cart-context';
 
 const HeaderCartButton = (props) => {
+
+    // Setting our state so we animate our button once we update the cart only
+    const [animateButton, setAnimateButton] = useState(false);
     
     // Importing our current cart context so we can share the data across the entire app
     const globalCartContext = useContext(CartContext);
@@ -11,12 +14,32 @@ const HeaderCartButton = (props) => {
     // Show our Cart modal on click
     const showModal = () => { props.showModal(true); }
 
+    const { items } = globalCartContext ;
+
     const numberOfCartItems = globalCartContext.items.reduce((prevNumber, item) => {
         return prevNumber + item.amount;
     }, 0);
 
+    useEffect( () => {
+
+        // Do nothing if our cart is empty ( such as initialization )
+        if( items.length === 0 ){ return; }
+
+        setAnimateButton(true);
+
+        // Remove the pulse animation by changing state after the animation ends
+        const timer = setTimeout( () => {
+            setAnimateButton(false);
+        }, 1001);
+
+        // Clean up our timer in case a new one is set
+        return () => clearTimeout(timer);
+        
+    },[items]);
+
+
     return(
-    <button className="header-cart" onClick={ showModal }>
+    <button className={ `header-cart ${ animateButton === true ? 'pulse' : '' }` } onClick={ showModal }>
         <CartIcon width="24" height="24"></CartIcon>
         <p> Your Cart </p>
         <div className="order-count"> { numberOfCartItems } </div>
